095a631f379bff5a7289dc79a5d7d5d7132fc4b3
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2015-11-07 02:50:46 +0100

    Initial commit v0.1.0

---
+++ README.md
@@ -0,0 +1,30 @@
+# svg.screenbbox.js
+
+A plugin for the [svgjs](https://github.com/wout/svg.js) library which gets the bbox of a path/polygon/polyline after all transformations applied.
+
+The returned bounding box is in the screens coordinate space which means that the scroll offset is not applied (same as with screenCTM)
+
+```javascript
+
+// add some viewbox madness to make this more interesting
+var nested = draw.viewbox(0, 0, 300, 200).nested().viewbox(-50, 20, 200, 300)
+
+// create path with transformation
+var path = draw.path('M150 0 L75 200 L225 200 Z').rotate(25).scale(0.5)
+
+// get bounding box in screen coordinates
+var bbox = path.screenbbox()
+
+// do something with it, e.g. draw a reactangle around (with jQuery here)
+$('<div>').css({
+  position:'absolute',
+  width:bbox.width,
+  height:bbox.height,
+  left:bbox.x+window.pageXOffset, // dont forget to add the scroll offset
+  top:bbox.y+window.pageYOffset,  // dito
+  outline:'3px solid black'
+}).appendTo('body')
+```
+
+## Dependencies
+This module requires svg.js >= v2.2.0
---
+++ src/svg.screenBBox.js
@@ -0,0 +1,82 @@
+SVG.extend(SVG.Polyline, SVG.Polygon, SVG.Path, {
+
+  screenBBox: function(){
+  
+    var arr = this.array().valueOf()          // path/point array
+      , i = arr.length                        // length of array, we need it later
+      , p = this.doc().node.createSVGPoint()  // instance of SVGPoint
+      , m = this.screenCTM().native()         // matrix for transfomation
+      , pos = [0,0]                           // default position (for V and H in Path)
+      , transformPoints = function(arr){      // function to transform all points in arr (returns the last)
+    
+      var transP
+    
+      // loop trough all coordinates and transform them
+      for(var i = 1, len = arr.length; i < len; i+=2){
+        p.x = arr[i]
+        p.y = arr[i+1]
+        
+        transP = p.matrixTransform(m)
+        
+        arr[i]   = transP.x
+        arr[i+1] = transP.y
+      }
+      
+      // return last computed values
+      return [transP.x, transP.y]
+    
+    }
+
+    // in case of polygon or polyline we need to normalize it to a path
+    if(this instanceof SVG.Polygon || this instanceof SVG.Polyline){
+
+      // just add the L
+      while(i--){
+        arr[i].unshift('L')
+      }
+      
+      // first has to be M
+      arr[0][0] = 'M'
+    
+    }
+    
+    // in case of polygon close the path accordingly
+    if(this instanceof SVG.Polygon){
+      arr.push(['z'])
+    }
+    
+    // reinitiate i cause we already used it
+    i = arr.length
+    while(i--){
+      
+      // get path segment
+      var s = arr[i][0]
+
+      // coordinate list
+      if (s == 'M' || s == 'L' || s == 'C' || s == 'S' || s == 'Q' || s == 'T'){
+        pos = [arr[i][1], arr[i][2]]
+        transformPoints(arr[i])
+      // only y
+      }else if (s == 'H'){
+        pos[0] = arr[i][1]
+        arr[i][1] = transformPoints([pos.slice()])[0]
+      // only x
+      }else if (s == 'V'){
+        pos[1] = arr[i][1]
+        arr[i][1] = transformPoints([pos.slice()])[1]
+      // position is the last one in arc
+      }else if (s == 'A'){
+        pos = [arr[i][6], arr[i][7]]
+        var transP = transformPoints([pos.slice()])
+        arr[i][6] = transP[0]
+        arr[i][7] = transP[1]
+      }
+      
+    }
+
+    // return bbox of created path array
+    return (new SVG.PathArray(arr)).bbox()
+  
+  }
+
+})
