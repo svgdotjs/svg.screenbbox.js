6f7a984914a2d07f81588fb0c620d949a4706858
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2015-11-08 18:48:25 +0100

    Fixes reference issue mentioned in https://github.com/wout/svg.js/issues/397 and cleans up code

--- src/svg.screenbbox.js
+++ src/svg.screenbbox.js
@@ -2,29 +2,32 @@ SVG.extend(SVG.Polyline, SVG.Polygon, SVG.Path, {
 
   screenBBox: function(){
 
-    var arr = this.array().valueOf()          // path/point array
-      , i = arr.length                        // length of array, we need it later
-      , p = this.doc().node.createSVGPoint()  // instance of SVGPoint
-      , m = this.screenCTM().native()         // matrix for transfomation
+    var i, transP
+      , arr = this.array().valueOf().slice()   // path/point array
+      , len = arr.length                       // length of array, we need it later
+      , p   = this.doc().node.createSVGPoint() // instance of SVGPoint
+      , m   = this.screenCTM().native()        // matrix for transfomation
       , pos = [,0,0]                           // default position (for V and H in Path)
-      , transformPoints = function(arr){      // function to transform all points in arr (returns the last)
+      , transformPoints = function(arr){       // function to transform all points in arr
 
-      var transP
-
-      // loop trough all coordinates and transform them
+      // loop trough all coordinates and transform them by reference
       for(var i = 1, len = arr.length; i < len; i+=2){
+
         p.x = arr[i]
         p.y = arr[i+1]
 
+        // transP is later used to get the last computed value
         transP = p.matrixTransform(m)
 
         arr[i]   = transP.x
         arr[i+1] = transP.y
       }
 
-      // return last computed values
-      return [transP.x, transP.y]
-
+    }
+    
+    // kill all refereces to get a clean clone of the array
+    for(i = 0; i < len; ++i){
+      arr[i] = arr[i].slice()
     }
 
     // in case of polygon or polyline we need to normalize it to a path
@@ -46,7 +49,7 @@ SVG.extend(SVG.Polyline, SVG.Polygon, SVG.Path, {
     }
 
     // loop through path array and transform all points
-    for(i = 0, len = arr.length; i < len; ++i){
+    for(i = 0; i < len; ++i){
 
       // get path segment
       var s = arr[i][0]
@@ -58,16 +61,16 @@ SVG.extend(SVG.Polyline, SVG.Polygon, SVG.Path, {
       // We have to transform it to Line cause vertical and horiz0ntal lines could be rotated
       }else if (s == 'H' || s == 'V'){
         pos[s == 'H' ? 1 : 2] = arr[i][1]
-        var transP = transformPoints(pos.slice())
+        transformPoints(pos.slice())
         arr[i][0] = 'L'
-        arr[i][1] = transP[0]
-        arr[i][2] = transP[1]
+        arr[i][1] = transP.x
+        arr[i][2] = transP.y
       // position is the last one in arc
       }else if (s == 'A'){
         pos = [,arr[i][6], arr[i][7]]
-        var transP = transformPoints(pos.slice())
-        arr[i][6] = transP[0]
-        arr[i][7] = transP[1]
+        transformPoints(pos.slice())
+        arr[i][6] = transP.x
+        arr[i][7] = transP.y
       }
 
     }
